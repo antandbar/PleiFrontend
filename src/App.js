f2e0cf93c4e120cbb060e554c66eb61a775f13d1
Antonio Andreu <antonio.andreu.barreiro@gmail.com>
2022-03-25 11:25:26 +0100

    feat: Implentar RequireAuth

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 import './index.css';
 import InternalUser from './views/internalUserList/InternalUserList.js';
@@ -11,35 +11,69 @@ import Error404 from './views/error/Error404';
 import { constNames } from './const/index.js';
 import LoginPage from './views/auth/LoginPage/LoginPage';
 import { AuthContextProvider } from './views/auth/context';
-
+import RequireAuth from './views/auth/RequireAuth';
 
 function App({ isInitiallyLogged }) {
-    const [isLogged, setIsLogged] = useState(isInitiallyLogged);
-  
-    const handleLogin = () => {
-      setIsLogged(true);
-    };
-  
-    const handleLogout = () => {
-      setIsLogged(false);
-    };
+  const [isLogged, setIsLogged] = useState(isInitiallyLogged);
 
+  const handleLogin = () => {
+    setIsLogged(true);
+  };
 
-return(
+  const handleLogout = () => {
+    setIsLogged(false);
+  };
 
-      <div>  
+  return (
+    <div>
       <AuthContextProvider value={{ isLogged, handleLogin, handleLogout }}>
         <Routes>
-        <Route path={constNames.routeNames.internalUser} element={<InternalUser />} />
-          <Route path={`${constNames.routeNames.internalUserDetail}/:id`} element={<InternalUserDetail/>}/>
-          <Route path={constNames.routeNames.internalCreateUser} element={<InternalCreateUser/>}/>
-          <Route path={`${constNames.routeNames.internalEditUser}/:id`} element={<InternalEditUser/>}/>
-          <Route path={constNames.routeNames.login} element={<LoginPage/>} />
-          <Route element={<Error404/>}/> 
+          <Route
+            path={constNames.routeNames.internalUser}
+            element={
+              <RequireAuth>
+                <InternalUser />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path={`${constNames.routeNames.internalUserDetail}/:id`}
+            element={
+              <RequireAuth>
+                <InternalUserDetail />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path={constNames.routeNames.internalCreateUser}
+            element={
+              <RequireAuth>
+                <InternalCreateUser />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path={`${constNames.routeNames.internalEditUser}/:id`}
+            element={
+              <RequireAuth>
+                <InternalEditUser />
+              </RequireAuth>
+            }
+          />
+          <Route path={constNames.routeNames.login} element={<LoginPage />} />
+          <Route
+            path="/404"
+            element={
+              <RequireAuth>
+                <Error404 />
+              </RequireAuth>
+            }
+          />
           <Route path="/" element={<Navigate to="/index" />} />
+          <Route path="*" element={<Navigate to="/404" />} />
         </Routes>
-        </AuthContextProvider>
-      </div>  
-);
+      </AuthContextProvider>
+    </div>
+  );
 }
 export default App;
